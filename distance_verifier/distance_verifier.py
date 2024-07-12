b0ee347c0519d0ebba7cb8579d51104993c335e5
Allan11231123 <allan098792@gmail.com>
2024-07-12 16:41:54 +0800

    Remove unused info logger and update image saving condition.

--- distance_verifier/distance_verifier.py
+++ distance_verifier/distance_verifier.py
@@ -65,6 +65,7 @@ class DistanceVerifier(Node):
         )
 
         self.groundtruth = None
+        self.checkpoint = None
         self.prediction = None
         self.difference = None
         self.image = None
@@ -84,7 +85,6 @@ class DistanceVerifier(Node):
 
     def yabloc_path_listener_callback(self, msg):
         self.prediction = msg.poses[-1].pose.position
-        # self.update_path_position()
     def carla_path_listener_callback(self,msg):
         self.groundtruth = msg.pose.position
         self.update_path_position()
@@ -98,6 +98,10 @@ class DistanceVerifier(Node):
             self.fde_publisher.publish(self.fde)
         if self.difference_value is not None:
             self.difference_publisher.publish(self.difference_value)
+        if self.checkpoint is not None and self.checkpoint==self.groundtruth:
+            self.save_image("result.png")
+        else:
+            self.checkpoint = self.groundtruth
         
 
     def calculate_difference(self):
@@ -105,7 +109,6 @@ class DistanceVerifier(Node):
         y_diff = self.prediction.y - self.groundtruth.y
         z_diff = self.prediction.z - self.groundtruth.z
         diff_value = (x_diff**2 + y_diff**2 + z_diff**2)**(1/2)
-        # print("The difference between groundtruth and prediction pose is {}".format(diff_value))
         return diff_value
     
     def save_image(self,name = None):
@@ -124,16 +127,13 @@ class DistanceVerifier(Node):
         ax.plot(self.time,self.differences)
         fig.canvas.draw()
         image = np.array(fig.canvas.renderer.buffer_rgba())
-        # buf = fig.canvas.tostring_rgb()
-        # cols, rows = fig.canvas.get_width_height()
-        # image = np.frombuffer(buf,dtype=np.int8).reshape(rows,cols,3)
         self.image = self.bridge.cv2_to_imgmsg(cv2.cvtColor(image,cv2.COLOR_RGBA2BGR),"bgr8")
         plt.close()
     
     def calculate_ade(self):
         ade_value = String()
-        ade_value.data = "=== ADE ===\n" + str(self.difference)
-        # ade_value.data = "=== ADE ===\n" + str(np.mean(cdist(self.predictions,self.groundtruths)))
+        # ade_value.data = "=== ADE ===\n" + str(self.difference)
+        ade_value.data = "=== ADE ===\n" + str(np.mean(cdist(self.predictions,self.groundtruths)))
         self.ade =  ade_value
     def calculate_fde(self):
         fde_value = String()
@@ -147,13 +147,11 @@ class DistanceVerifier(Node):
     def update_path_position(self):
         if self.groundtruth is None or self.prediction is None:
             return
-        self.get_logger().info("Recieve ground truth position and calculate distance difference.")
         diff = self.calculate_difference()
         if self.difference == diff: 
             return
         else:
             self.difference = diff
-            self.get_logger().info("The difference between ground truth pose and prediction pose is {}.".format(self.difference))
             self.generate_difference_value()
             self.differences.append(self.difference)
             self.groundtruths.append([self.groundtruth.x, self.groundtruth.y, self.groundtruth.z])
@@ -171,7 +169,6 @@ def main():
     distance_verifier = DistanceVerifier()
 
     rclpy.spin(distance_verifier)
-    distance_verifier.save_image("result.png")
     distance_verifier.destroy_node()
     rclpy.shutdown()
 
